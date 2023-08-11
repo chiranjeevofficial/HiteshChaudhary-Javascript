bbc1be3a5a3771bacf9d5105f6f0c878fb8873ff
Chiranjeev Kashyap <87925969+chiranjeevofficial@users.noreply.github.com>
2023-08-11 22:29:11 +0530

    data-types

---
+++ script-003.js
@@ -0,0 +1,33 @@
+// data-types and ECMA Standards
+
+"use strict"; // treat all JS code as newer version
+
+let name = "chiranjeev"
+let age = 20
+let isLoggedIn = true
+let state
+// alert(3 + 3) // there is in node have different syntax to use it
+// we're using nodejs, not browser
+
+// console.log(3+3);
+
+// console.log("Namaste");
+
+// datatypes
+// number => 2^53
+// bigInt
+// string = ""
+// boolean => true/false
+// null (its stand-alone value)
+// undefined =>
+// symbol => unique
+
+// object
+// typeof
+
+console.log(typeof "namaste");
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+console.log(typeof null);
+console.log(typeof undefined);
