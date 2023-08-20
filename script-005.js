7812b4cebfc5b9f4b92478785c6542d5b0996fa9
Chiranjeev Kashyap <87925969+chiranjeevofficial@users.noreply.github.com>
2023-08-20 11:00:23 +0530

    operations

---
+++ script-005.js
@@ -0,0 +1,33 @@
+// operations
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3); //x is to the power y
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+// let str1 = "Namaste "
+// let str2 = "Coder"
+// console.log(str1 + str2);
+
+// console.log("1" + "2");
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2"); 
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
