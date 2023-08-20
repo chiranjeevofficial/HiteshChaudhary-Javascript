7a4e6c00f3057db4f6786c5f98a9ac9ba693b141
Chiranjeev Kashyap <87925969+chiranjeevofficial@users.noreply.github.com>
2023-08-20 12:15:27 +0530

    memory allocation in js

---
+++ script-008.js
@@ -0,0 +1,22 @@
+// memory allocation in javascript
+// Stack(Primitive) Heap(Non-Primitive)
+
+// let myName = "Chiranjeev";
+// let fullName = myName;
+// fullName = "Chiranjeev Kashyap";
+
+// console.log(myName);
+// console.log(fullName);
+
+// let userOne = {
+//     email: "usermail@gmail.com",
+//     upi: "user@ybl"
+// }
+
+// let userTwo = userOne;
+
+// userTwo.email = "chiranjeev@google.com"
+
+// console.log(userOne.email);
+// console.log(userTwo.email);
+
