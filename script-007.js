11bcf9a00e0162be0ef041c43ca9a651563f615f
Chiranjeev Kashyap <87925969+chiranjeevofficial@users.noreply.github.com>
2023-08-20 11:45:28 +0530

    data-type summary

---
+++ script-007.js
@@ -0,0 +1,32 @@
+// data-types
+// Primitive
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// let score = 100;
+// let scoreValue = 100.3;
+// let isLOggedIn = false;
+// let outsideTemp = null;
+// let userEmail = undefined;
+// let id = Symbol('123');
+// let anotherId = Symbol('12345');
+// console.log(id === anotherId);
+
+// let bigNumber = 7983499388n;
+// console.log(typeof bigNumber);
+
+// Reference (Non-Primitive)
+// Array, Objects, Functions
+// let webSeries = ["Mirzapur", "Ashram", "Raktanchal"]
+// console.log(webSeries);
+
+// let peronObj = {
+    // name: "Chiranjeev",
+    // age: 22
+// }
+// console.log(peronObj);
+
+let myFunction = function() {
+    console.log("Namaste Javascript");
+}
+
+myFunction();
