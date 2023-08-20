ba1ad059bd3d0605af281e9187dd293efeb5d07d
Chiranjeev Kashyap <87925969+chiranjeevofficial@users.noreply.github.com>
2023-08-20 10:17:55 +0530

    data type conversion

---
+++ script-004.js
@@ -0,0 +1,13 @@
+// data-types conversion
+
+let score = 30
+// console.log(typeof score);
+
+score = "30"
+// console.log(typeof score);   
+
+let value = Number(score)
+// console.log(typeof value);
+
+value = Number("30abc")
+console.log(typeof value);
