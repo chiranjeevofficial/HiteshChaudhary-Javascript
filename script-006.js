1b2b791f0675deb2a877a224f05c7f4c124232e1
Chiranjeev Kashyap <87925969+chiranjeevofficial@users.noreply.github.com>
2023-08-20 11:28:29 +0530

    comparison

---
+++ script-006.js
@@ -0,0 +1,24 @@
+// comparison of datatypes
+
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// comparison convert null to a number, treating it as 0.
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined == null);
+
+// === strictly check
+console.log("2" === 2);
+console.log("2" === "2");
