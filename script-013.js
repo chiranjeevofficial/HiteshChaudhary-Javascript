7ed36ee6a923eaa1ad294a78a5b8a0054df3caa9
Chiranjeev Kashyap <87925969+chiranjeevofficial@users.noreply.github.com>
2023-08-22 21:24:35 +0530

    object advance

--- script-013.js
+++ script-013.js
@@ -3,18 +3,18 @@
 // Object.create(): object create using constructor.
 
 // object lierals
-let mySym = Symbol("Key1");
+// let mySym = Symbol("Key1");
 
-const JsUser = {
-    name: "Chiranjeev",
-    "full name": "Chiranjeev Kashyap",
+// const JsUser = {
+    // name: "Chiranjeev",
+    // "full name": "Chiranjeev Kashyap",
     // mySym: "Key2", (its not appropriate way)
-    [mySym]: "Key2",
-    age: 22,
-    location: "rishikesh",
-    email: "chiranjeev@google.com",
-    isLoggedIn: false
-};
+    // [mySym]: "Key2",
+    // age: 22,
+    // location: "rishikesh",
+    // email: "chiranjeev@google.com",
+    // isLoggedIn: false
+// };
 
 // JsUser.name (its not appropriate way to access the object)
 // console.log(JsUser["full name"]); //(its appropriate way)
@@ -39,8 +39,47 @@ const JsUser = {
 // console.log(JsUser.greeting);
 // console.log(JsUser.greeting());
 
-JsUser.getName = function() {
-    console.log(`Namaste JsUser, ${this["full name"]}`);
-}
+// JsUser.getName = function() {
+    // console.log(`Namaste JsUser, ${this["full name"]}`);
+// }
 
-console.log(JsUser.getName());
+// console.log(JsUser.getName());
+
+// ============== Advance Object ==============
+
+// let tinderUser = new Object(); //key difference is this is singleton object
+// let tinderUser = {}
+
+// let tinderUser = {};
+// tinderUser.id = "TU123";
+// tinderUser.name = "Naman";
+// tinderUser.isLoggedIn = true;
+// console.log(tinderUser);
+
+// let facebookUser = {
+    // email: "someone@google.com",
+    // fullName: {
+        // userFullName: {
+            // firstName: "Chiranjeev",
+            // lastName: "kashyap"
+        // }
+    // }
+// }
+// console.log(facebookUser); //complete object
+// console.log(facebookUser.fullName); //nested-object
+// console.log(facebookUser.fullName.userFullName.firstName); //nested-object
+// console.log(facebookUser.fullName.userFullName.lastName); //nested-object
+
+// let obj1 = {1: "a", 2: "b", 3: "c"}
+// let obj2 = {3: "a", 4: "b", 5: "c"}
+
+// let obj3 = {obj1, obj2}
+// let obj3 = Object.assign({}, obj1, obj2)
+
+// let obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+// console.log(Object.values(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
