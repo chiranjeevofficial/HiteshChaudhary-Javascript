0bce2cf1a98425b5b6beb8c7789182f0a290d28a
Chiranjeev Kashyap <87925969+chiranjeevofficial@users.noreply.github.com>
2023-08-22 19:11:29 +0530

    date and time

---
+++ script-011.js
@@ -0,0 +1,22 @@
+// date-time
+
+// let date = new Date();
+// console.log(date);
+// console.log(date.toString());
+// console.log(date.toISOString());
+// console.log(date.toUTCString());
+// console.log(date.toDateString());
+// console.log(date.toTimeString());
+// console.log(date.toLocaleString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleTimeString());
+// console.log(typeof date);
+
+// let newDate = new Date(2023, 11, 23, 4, 15);
+// let newDate = new Date("08-22-2023");
+// console.log(newDate.toLocaleString());
+
+let myTimeStamp = new Date();
+console.log(myTimeStamp.getMonth() + 1);
+console.log(myTimeStamp.getUTCFullYear());
+console.log(myTimeStamp.getDate());
