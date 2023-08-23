2445151b2c22bba8acc3c497febb47f31cbe54ec
Chiranjeev Kashyap <87925969+chiranjeevofficial@users.noreply.github.com>
2023-08-23 22:36:11 +0530

    high order array loop

---
+++ script-021.js
@@ -0,0 +1,99 @@
+// high order array loop (below loop are mostly array specific loop)
+
+// for of loop
+// let arr = [1, 2, 3, 4, 5];
+// let arr = "Namaste Coder";
+// for (const element of arr) {
+    // console.log(element);
+// }
+
+// ============== Maps ============== 
+// map are contain only unique value
+// let map = new Map();
+// map.set("in", "India");
+// map.set("en", "England");
+// map.set("ch", "China");
+// map.set("jp", "Japan");
+// console.log(map);
+
+// for (const key of map) {
+// in below, [] is destructure operator
+// for (const [key, value] of map) {
+    // console.log(key, ":-", value);
+// }
+
+// let gameObject = {
+//     "game1": "kabaddi",
+//     "game2": "kushti",
+//     "game3": "dangal",
+// }
+// for (const [key, value] of gameObject) {
+//     console.log(key, ":-", value); //gameObject is not iterable
+// }
+
+// let course = {
+//     c: "C",
+//     cpp: "C++",
+//     py: "Python",
+//     java: "Java",
+//     js: "javascript"
+// }
+// for (const key in course) {
+//     // console.log(key);
+//     // console.log(course[key]);
+//     console.log(`${key} shortcut is for ${course[key]}`);
+// }
+
+// const programming = ["c", "cpp", "python", "java", "javascript", "html", "css"];
+// 
+// for (const key in programming) {
+    // // console.log(`${key} key for value is ${programming[key]}`);
+// }
+
+// let map = new Map();
+// map.set("in", "India");
+// map.set("en", "England");
+// map.set("ch", "China");
+// map.set("jp", "Japan");
+// 
+// for (const key in map) {
+    // console.log(key);
+// }
+
+// const programming = ["c", "cpp", "python", "java", "javascript", "html", "css"];
+// for-each cannot have the function name
+// programming.forEach(function (item) {
+//     console.log(item);
+// })
+
+// programming.forEach((item) => {
+//     console.log(item);
+// })
+
+// function printMe(item) {
+    // console.log(item);
+// }
+// programming.forEach(printMe);
+
+// programming.forEach( (item, index, arr) => {
+//     console.log(item, index, arr);
+// })
+
+let coding = [
+    {
+        name: "Java",
+        fileName: "java"
+    },
+    {
+        name: "JavaScript",
+        fileName: "js"
+    },
+    {
+        name: "Python",
+        fileName: "py"
+    },
+]
+
+coding.forEach( (item) => {
+    console.log(`${item.name} extension is .${item.fileName}`);
+})
