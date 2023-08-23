8e9e992afc7a504a6c4a6be7e4d1ce5f638130b9
Chiranjeev Kashyap <87925969+chiranjeevofficial@users.noreply.github.com>
2023-08-23 11:29:15 +0530

    scope

---
+++ script-015.js
@@ -0,0 +1,21 @@
+// scope
+
+// const a = 10;
+// let b = 20;
+// var c = 30;
+
+// var c = 300; 
+
+let a = 300;
+
+if (true) {
+    const a = 10;
+    // let b = 20;
+    // var c = 30;
+    // c = 30;
+    console.log(a);
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
