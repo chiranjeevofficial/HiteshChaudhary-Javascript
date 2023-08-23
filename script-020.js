3d43c9b18dda33649e6f415015a2aedbefef0259
Chiranjeev Kashyap <87925969+chiranjeevofficial@users.noreply.github.com>
2023-08-23 21:50:21 +0530

    while or do-while

--- script-020.js
+++ script-020.js
@@ -1,14 +1,37 @@
 // iteration with break and continue
 // for loop
-for (let i = 0 ; i <= 10 ; i++) {
+// for (let i = 0 ; i <= 10 ; i++) {
     // console.log(`Namaste ${i} time`);
-    if (i == 7) {
-        console.log(`Hurry, i find ${i}`);
-        break;
-    }
-    if (i == 5) {
-        console.log(`I detect ${i}`);
-        continue;
-    }
-    console.log(`${i}`);
-}
+    // if (i == 7) {
+        // console.log(`Hurry, i find ${i}`);
+        // break;
+    // }
+    // if (i == 5) {
+        // console.log(`I detect ${i}`);
+        // continue;
+    // }
+    // console.log(`${i}`);
+// }
+
+// while and do-while loop
+// let index = 0;
+// while (index <= 10) {
+    // console.log(`Value of Index is: ${index}`);
+    // index = index + 1;
+    // index += 1;
+    // index++;
+// }
+
+// let myArray = [10, 5, 20, 15, 30, 25, 40, 35, 50, 45];
+// let index = 0;
+// while (index < myArray.length) {
+    // console.log(`Array ${index} value is: ${myArray[index]}`);
+    // index++;
+// }
+
+// let score = 1;
+let score = 11;
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
